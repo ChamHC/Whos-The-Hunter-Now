b7a0b1d83583b799cbd179332b308625abf578a8
lutfiizzat <lutfi.izzat3@gmail.com>
2024-05-14 09:56:48 +0800

    cleaned code

--- scripts/Platform.js
+++ scripts/Platform.js
@@ -8,9 +8,8 @@ export default class Platform {
 
     create(){
         this.map = this.scene.make.tilemap({ key: 'platform_map' });
-        const tileset = this.map.addTilesetImage('HuterTileset', 'tiles');
+        const tileset = this.map.addTilesetImage('HunterTileset', 'tiles');
         this.platforms = this.map.createLayer('Platform', tileset, 0, 0); // Assigning to instance property
-        console.log("fucker " + this.platforms.x);
     
         this.platforms.setCollisionByExclusion(-1, true);
         this.scene.physics.add.collider(this.playerA.sprite, this.platforms);
@@ -23,22 +22,20 @@ export default class Platform {
 
     }
     
-    getWaypoints(){
-        const waypoints = []; // Array to store all waypoints
+    getWaypoints(name){
+        const waypoints = [];
     
-        const waypointLayer = this.map.getObjectLayer('Spawner');
+        const waypointLayer = this.map.getObjectLayer(name);
     
         if (waypointLayer) {
             // Iterate over waypoints
             waypointLayer.objects.forEach(waypoint => {
                 // Access custom properties of waypoint object
                 const { x, y } = waypoint;
-    
-                // Store waypoint data in the array
                 waypoints.push({ x, y });
             });
         }
-        return waypoints; // Return the array of waypoints
+        return waypoints; 
     }  
 }
 
